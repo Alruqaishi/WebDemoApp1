e57ecfe5eeb1d78699ff3eb262dce7d0e5ef5b86
Sulaiman Al Ruqaishi <s.alruqaishi@hotmail.com>
2022-06-03 16:33:54 +0400

    initial commit

---
+++ src/main/java/com/SpringFramework/WebDemoApp1/WebDemoApp1Application.java
@@ -0,0 +1,13 @@
+package com.SpringFramework.WebDemoApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDemoApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDemoApp1Application.class, args);
+	}
+
+}
